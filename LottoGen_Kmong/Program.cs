dad5e353ba0fb12e2bd6b4296fa069766220c0bb
ingyeoking13 <ingyeoking13@gmail.com>
2019-02-12 14:07:30 +0900

    초기 커밋.

---
+++ LottoGen_Kmong/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LottoGen_Kmong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
